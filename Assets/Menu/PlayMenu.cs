0c5e062bdac83c2053240bf51b9a2454b4d3776b
Thomas Sayer <thomas.sayer@outlook.fr>
2018-11-25 15:50:50 -0500

    TitleScreen + Menu

---
+++ Assets/Menu/PlayMenu.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayMenu : MonoBehaviour {
+
+	public void Play() {
+		SceneManager.LoadScene("Play/Play");
+	}
+
+	public void Back() {
+		SceneManager.LoadScene("Menu/Menu");
+	}
+}
---
+++ Assets/TitleScreen/KeyPress.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class KeyPress : MonoBehaviour {
+
+	void Update () {
+		if (Input.anyKey) {
+			SceneManager.LoadScene("Menu/Menu");
+        }
+	}
+}
